29a0c3bbbcb19f0cdf9f61d9225ebce894967186
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-07-14 00:30:47 +0200

    finish unit test for authSlice

--- src/tests/fixtures/authFixture.js
+++ src/tests/fixtures/authFixture.js
@@ -16,13 +16,22 @@ export const authenticaed= {
   errorMessage: null,
 }
 
+export const checking = {
+  status: 'checking', // 'checking', 'not-authenticated', 'authenticated'
+  uid: 'valid-uid',
+  email: 'test@gmail.com',
+  displayName: 'user-prueba',
+  photoURL: 'some-pic-url',
+  errorMessage: null,
+}
+
 export const notAuthenticaed= {
   status: 'not-authenticated', // 'checking', 'not-authenticated', 'authenticated'
   uid: null,
   email: null,
   displayName: null,
   photoURL: null,
-  errorMessage: null,
+  errorMessage: undefined,
 }
 
 export const userTest = {
--- src/tests/store/auth/authSlice.test.js
+++ src/tests/store/auth/authSlice.test.js
@@ -1,11 +1,27 @@
 
-import { initialState } from '../../fixtures/authFixture';
-import { authSlice } from './../../../store/auth/authSlice';
+import { authenticaed, initialState, userTest } from '../../fixtures/authFixture';
+import { authSlice, login, logout, checkingCredentials } from './../../../store/auth/authSlice';
+import { notAuthenticaed, checking } from './../../fixtures/authFixture';
 
 describe('authSlice scenarios:::::', () => {
   it('should load initialState', () => {
     const state = authSlice.reducer(initialState, {});
     expect(authSlice.name).toBe('auth');
     expect( state ).toEqual(initialState);
+  });
+
+  it('should make login succesfully', () => {
+    const state = authSlice.reducer(initialState, login(userTest));
+    expect(state).toEqual(authenticaed);
+  });
+
+  it('should make logout succesfully', () => {
+    const state = authSlice.reducer(initialState, logout());
+    expect(state).toEqual(notAuthenticaed);
+  });
+
+  it('should make checkingCredentials succesfully', () => {
+    const state = authSlice.reducer(initialState, checkingCredentials(userTest));
+    expect(state.status).toEqual('checking');
   });
 });
